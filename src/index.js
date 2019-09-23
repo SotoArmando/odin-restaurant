6bef5ebff5d3cb81a828974bcde17fdb74deee87
SotoArmando <armando29@live.com>
2019-09-22 21:55:07 -0400

    feature(functional): linter fix

--- src/index.js
+++ src/index.js
@@ -1,23 +1,42 @@
-document.querySelector("first-content").style.display = ""
-document.querySelector("second-content").style.display = "none"
-document.querySelector("third-content").style.display = "none"
 
-const openHome  = () => {
-    document.querySelector("first-content").style.display = ""
-    document.querySelector("second-content").style.display = "none"
-    document.querySelector("third-content").style.display = "none"
+const content1 = `<first-content>
+<img src="svg/baker.svg" alt="Kiwi standing on oval" width="225" style="display: block;align-self: center;">
+<span>
+    Hi! am <b>cheftie</b> welcome to my bussines. this is my Restaurant and am very happy to see you here :)
+    ask me anything * we got no that so ... , just order if you like some water :D .
+    <input type="button" value="Order water :D" />
+    <input type="button" value="Order something else" />
+</span>
+</first-content>
+`
+
+const content2 = `<second-content>
+<img src="svg/baker.svg" alt="Kiwi standing on oval" width="225" style="display: block;align-self: center;">
+<span>
+    We good doing all you can ask to :D, rigth now not but it is cause there is no more ingredients srry ;s.
+</span>
+</second-content>
+`
+
+const content3 = `<third-content>
+<img src="svg/baker.svg" alt="Kiwi standing on oval" width="225" style="display: block;align-self: center;">
+<span>
+   You can find us next corner ya see this day. well be there ... i promise :v :D.
+</span>
+</third-content>`
+
+document.querySelector("wrapper").innerHTML = content1;
+
+const openHome = () => {
+    document.querySelector("wrapper").innerHTML = content1;
 }
 
 const openAboutUs = () => {
-    document.querySelector("first-content").style.display = "none"
-    document.querySelector("second-content").style.display = ""
-    document.querySelector("third-content").style.display = "none"
+    document.querySelector("wrapper").innerHTML = content2;
 }
 
 const openContactUs = () => {
-    document.querySelector("first-content").style.display = "none"
-    document.querySelector("second-content").style.display = "none"
-    document.querySelector("third-content").style.display = ""
+    document.querySelector("wrapper").innerHTML = content3;
 }
 
 document.getElementById("OpenHome").addEventListener("click", openHome);
